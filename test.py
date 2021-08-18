404b397dab988393f92dda7944007b994fd309ca
Tirth Shah <tirth.shah@playpowerlabs.com>
2021-08-18 18:09:21 +0530

    testing test.py

--- test.py
+++ test.py
@@ -5,3 +5,16 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 install("neuspell")
+install("jiwer")
+
+import neuspell
+neuspell.seq_modeling.downloads.download_pretrained_model("subwordbert-probwordnoise")
+
+from neuspell import BertChecker
+checker = BertChecker()
+checker.from_pretrained()
+
+subprocess.check_call("apt-get -y install sox ffmpeg")
+subprocess.check_call("pip install transformers ffmpeg-python sox")
+subprocess.check_call("wget https://raw.githubusercontent.com/harveenchadha/bol/main/demos/colab/record.py")
+
